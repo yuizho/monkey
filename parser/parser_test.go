057ee349032164c07bf29098de8c44cbbc817b10
yuizho <yuizho3@gmail.com>
2021-08-15 14:15:33 +0900

    add return statement parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,6 +7,35 @@ import (
 	"github.com/yuizho/monkey/lexer"
 )
 
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statemtns. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatemen. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
+	}
+}
+
 func TestStatements(t *testing.T) {
 	input := `
 		let x = 5;
