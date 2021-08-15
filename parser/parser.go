f936c0ef00d9dc9954483f7921b347913c38f989
yuizho <yuizho3@gmail.com>
2021-08-15 12:54:02 +0900

    add error check feature to parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,21 +1,26 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/yuizho/monkey/ast"
 	"github.com/yuizho/monkey/lexer"
 	"github.com/yuizho/monkey/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-
+	l      *lexer.Lexer
+	errors []string
 	//  pointers of lexer instance
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
@@ -52,6 +57,15 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
@@ -81,11 +95,13 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 	return p.peekToken.Type == t
 }
 
+// assertion function!!!
 func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
