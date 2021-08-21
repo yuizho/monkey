03d70bc79929d74d53fe67ff7d3143f291e9195b
yuizho <yuizho3@gmail.com>
2021-08-21 20:41:49 +0900

    modify evaluator to add bool

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/yuizho/monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// statement
@@ -15,11 +20,20 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 
 	// expression
+	case *ast.Boolean:
+		return nativeBooleanObject(node.Value)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 
 	return nil
+}
+
+func nativeBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
