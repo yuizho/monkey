73e3c094a6ff8858426ca13560040123acd86cca
yuizho <yuizho3@gmail.com>
2021-08-21 21:06:20 +0900

    modify evaluator to add ! operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 
 	// expression
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	case *ast.Boolean:
 		return nativeBooleanObject(node.Value)
 	case *ast.IntegerLiteral:
@@ -28,6 +31,28 @@ func Eval(node ast.Node) object.Object {
 	}
 
 	return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func nativeBooleanObject(input bool) *object.Boolean {
